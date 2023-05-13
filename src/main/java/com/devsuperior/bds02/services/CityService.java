3640eac59266d89158a758f13b4ea31e1f82cb53
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2023-05-13 00:28:02 -0300

    City tests
    Implemented delete based on tests:
    -deleteShouldReturnNoContentWhenIndependentId
    -deleteShouldReturnNotFoundWhenNonExistingId
    -deleteShouldReturnBadRequestWhenDependentI

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exception.DatabaseException;
+import com.devsuperior.bds02.services.exception.ResourceNotFoundException;
 
 @Service
 public class CityService {
@@ -27,5 +31,15 @@ public class CityService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CityDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
